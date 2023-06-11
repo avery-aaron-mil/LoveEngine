284ba82d8ecb6b0a4e210a027d1e1441155ef32f
Avery Aaron <business.loveai@gmail.com>
2023-06-11 12:33:54 +0300

    Created/edited build files

---
+++ build.bat
@@ -0,0 +1,25 @@
+@echo off
+set BUILD_TYPE="RELEASE"
+
+:: Compile shaders
+for %%F in (shaders/*) do (
+  glslc "src\shaders\%%F" -o "products\assets\shaders\%%F.spv"
+)
+
+:: Compile code and link libraries (-mwindows for no console)
+mkdir build
+cd build
+cmake ^
+ -G "MinGW Makefiles" ^
+ -DCMAKE_CXX_COMPILER=g++ ^
+ -DCMAKE_BUILD_TYPE="%BUILD_TYPE%" ^
+ ..
+mingw32-make
+
+:: Copy all outputs to products
+for /R . %%f in (*.dll) do copy %%f "..\products\bin\"
+copy "game.exe" "..\products\bin\"
+copy "host.exe" "..\products\bin\"
+copy "launcher.exe" "..\products\"
+
+PAUSE
