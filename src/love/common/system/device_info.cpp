f098fcc3530501bbc7044c5f84410b84d1fa6297
Avery Aaron <AiLovesAi@users.noreply.github.com>
2023-06-12 21:34:04 +0300

    Moved/edited deviceInfo code and suggested improvements
    Moved most of the code from skeleton engine here, but made it look a
    little neater and added some tips on how to improve when I have access
    to my laptop instead of my phone.
    Also added tips for custom a device info query on multiple systems.
    This class is likely going to see some big changes in the future!

--- src/love/common/system/device_info.cpp
+++ src/love/common/system/device_info.cpp
@@ -1,19 +1,122 @@
 #include "device_info.hpp"
 
+#include <lzma.h>
+
+#if defined(_WIN32)
+  #include <windows.h>
+#elif defined(__linux__)
+  #include <unistd.h>
+  #include <sys/utsname.h>
+  #include <iterator>
+  #include <stdlib.h>
+#elif defined(__APPLE__)
+  #include <mach-o/dyld.h>
+  #include <sys/utsname.h>
+  #include <iterator>
+  #include <stdlib.h>
+#else
+#error "OS not supported"
+#endif
+
 namespace love_engine {
     void Device_Info::_find_OS() const noexcept {
-        //
+        // TODO Use a string buffer and make this neater
+#if defined(_WIN32)
+        OS.append("Windows");
+
+        OSVERSIONINFOA versionInfo{};
+        versionInfo.dwOSVersionInfoSize = sizeof(OSVERSIONINFOA);
+        GetVersionExA(&versionInfo);
+
+        switch (versionInfo.dwMajorVersion) {
+            case 10:
+                switch (versionInfo.dwMinorVersion) {
+                    case 0:
+                        OS.append(" 10");
+                    break;
+                    default:
+                        OS.append(" v10 Other");
+                    break;
+                }
+            break;
+            case 6:
+                switch (versionInfo.dwMinorVersion) {
+                    case 3:
+                        OS.append(" 8.1");
+                    break;
+                    case 2:
+                        OS.append(" 8");
+                    break;
+                    case 1:
+                        OS.append(" 7");
+                    break;
+                    case 0:
+                        OS.append(" Vista");
+                    break;
+                    default:
+                        OS.append(" v6 Other");
+                    break;
+                }
+            break;
+            case 5:
+                OS.append(" XP");
+            break;
+            default:
+                OS.append(" Other");
+            break;
+        }
+
+        if (String::isAscii(versionInfo.szCSDVersion)) OS.append(versionInfo.szCSDVersion);
+#elif defined(__unix__)
+        struct utsname unameData;
+        uname(&unameData);
+        OS.append(nameData.sysname);
+        OS.append(' ');
+        OS.append(nameData.release);
+#else
+#error "OS not supported"
+#endif
+        System::_OS = OS.str();
     }
     
     void Device_Info::_find_CPU() const noexcept {
-        //
+        char buffer[sizeof(uint32_t) * (3 /* 8.2-8.4 */) * (4 /* RAX-RDX */) + 1];
+        // TODO Find a C++ way to do this that doesn't involve a for loop
+    
+        for(uint32_t i = 0x80000002; i <= 0x80000004; i++) {
+            CPU_ID cpu_ID(i, 0);
+            buffer << String::asAscii(reinterpret_cast<const char*>(&cpu_ID.RAX()), 4);
+            buffer << String::asAscii(reinterpret_cast<const char*>(&cpu_ID.RBX()), 4);
+            buffer << String::asAscii(reinterpret_cast<const char*>(&cpu_ID.RCX()), 4);
+            buffer << String::asAscii(reinterpret_cast<const char*>(&cpu_ID.RDX()), 4);
+        }
+        
+        _cpu_Name = std::string(buffer);
+    }
+    
+    void Device_Info::_find_CPU_Thread_Count() const noexcept {
+        _CPU_Thread_Count = lzma_cputhreads();
+        // TODO Check for errors or make custom device query
     }
     
     void Device_Info::_find_Video_Card() const noexcept {
-        //
+        // TODO Find a way to get this without Vulcan
     }
     
     void Device_Info::_find_Physical_Memory() const noexcept {
-        //
+        _physical_Memory = lzma_physmem();
+        // TODO Check for errors or make custom device query
     }
+    
+    // TODO Remove underscores. they're ugly.
+    
+    // TODO Find audio, monitor, and disk devices, and voltage/temp info using
+    // sysconf and fscanf on hwmon (Linux), and GetSystemInfo and ManagementObjectSelector (Windows)
+    // Linux: https://stackoverflow.com/questions/150355/ and https://stackoverflow.com/questions/23716135/
+    // Windows: https://www.codeproject.com/Articles/17973/
+    
+    // TODO Get free space using statvfs (Linux) and GetDiskFreeSpaceA (Windows)
+    
+    // TODO Get battery info using GetSystemPowerStatus (Windows) and ACPID (Linux)
+    // https://stackoverflow.com/questions/27613517/querying-the-power-status-of-a-linux-machine-programmatically
 }
--- src/love/common/system/device_info.hpp
+++ src/love/common/system/device_info.hpp
@@ -1,5 +1,5 @@
-#ifndef LOVE_SYSTEM_INFO_HPP
-#define LOVE_SYSTEM_INFO_HPP
+#ifndef LOVE_DEVICE_INFO_HPP
+#define LOVE_DEVICE_INFO_HPP
 
 #include <cstdint>
 #include <string>
@@ -11,6 +11,7 @@ namespace love_engine {
             Device_Info() {
                 _find_OS();
                 _find_CPU();
+                _find_CPU_Thread_Count();
                 _find_Video_Card();
                 _find_Physical_Memory();
             }
@@ -18,12 +19,14 @@ namespace love_engine {
 
             const std::string& get_OS() const noexcept { return _OS_Name; }
             const std::string& get_CPU() const noexcept { return _CPU_Name; }
+            uint32_t get_CPU_Thread_Count() const noexcept { return _CPU_Thread_Count; }
             const std::string& get_Video_Card() const noexcept { return _video_Card_Name; }
             uint64_t get_Physical_Memory() const noexcept { return _physical_Memory; }
 
         private:
             std::string _OS_Name;
             std::string _CPU_Name;
+            uint64_t _CPU_Thread_Count;
             std::string _video_Card_Name;
             uint64_t _physical_Memory;
 
@@ -31,8 +34,28 @@ namespace love_engine {
             void _find_CPU() const noexcept;
             void _find_Video_Card() const noexcept;
             void _find_Physical_Memory() const noexcept;
+            
+            class CPU_ID {
+                uint32_t registers[4];
+
+                public:
+                explicit CPU_ID(unsigned func_Id, unsigned sub_Func_Id) {
+                    asm volatile(
+                        "cpuid"
+                        : "=a" (registers[0]), "=b" (registers[1]),
+                        "=c" (registers[2]), "=d" (registers[3])
+                        : "a" (funcId), "c" (subFuncId)
+                    );
+                    // RCX is set to zero for CPUID function 4
+                }
+
+                const uint32_t &RAX() const {return regs[0];}
+                const uint32_t &RBX() const {return regs[1];}
+                const uint32_t &RCX() const {return regs[2];}
+                const uint32_t &RDX() const {return regs[3];}
+            };
     };
 
 }
 
-#endif // LOVE_SYSTEM_INFO_HPP
+#endif // LOVE_DEVICE_INFO_HPP
