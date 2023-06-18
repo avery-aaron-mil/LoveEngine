6727fd16b5f858ec6311dacdb19050122f57c65e
Avery Aaron <business.loveai@gmail.com>
2023-06-18 09:05:10 +0300

    Added video card info

--- src/love/common/system/system_info.cpp
+++ src/love/common/system/system_info.cpp
@@ -56,9 +56,14 @@ namespace love_engine {
             IWbemClassObject* wbemClassObj = _wmi_Instance.get_Next_Query_Object(&queryResults);
             if (wbemClassObj) {
                 CPU_Info cpu;
+                cpu.name = _wmi_Instance.get_Object_Value_String(wbemClassObj, L"Name");
+                cpu.description = _wmi_Instance.get_Object_Value_String(wbemClassObj, L"Description");
+                cpu.socket = _wmi_Instance.get_Object_Value_String(wbemClassObj, L"SocketDesignation");
                 cpu.cores = _wmi_Instance.get_Object_Value_UI32(wbemClassObj, L"NumberOfCores");
                 cpu.threads = _wmi_Instance.get_Object_Value_UI32(wbemClassObj, L"NumberOfLogicalProcessors");
-                cpu.name = _wmi_Instance.get_Object_Value_String(wbemClassObj, L"Name");
+                cpu.speedMax = _wmi_Instance.get_Object_Value_UI32(wbemClassObj, L"MaxClockSpeed");
+                cpu.l2CacheSize = _wmi_Instance.get_Object_Value_UI32(wbemClassObj, L"L2CacheSize");
+                cpu.l3CacheSize = _wmi_Instance.get_Object_Value_UI32(wbemClassObj, L"L3CacheSize");
                 _CPUs.push_back(std::move(cpu));
                 wbemClassObj->Release();
             }
@@ -71,8 +76,31 @@ namespace love_engine {
 #endif
     }
     
-    void SystemInfo::_find_Video_Card() noexcept {
+    void SystemInfo::_find_Video_Cards() noexcept {
+#ifdef _WIN32
+        IEnumWbemClassObject* queryResults = _wmi_Instance.query_System_Info(L"SELECT * FROM Win32_VideoController");
+        do {
+            IWbemClassObject* wbemClassObj = _wmi_Instance.get_Next_Query_Object(&queryResults);
+            if (wbemClassObj) {
+                Video_Card_Info gpu;
+                gpu.name = _wmi_Instance.get_Object_Value_String(wbemClassObj, L"Name");
+                gpu.driverVersion = _wmi_Instance.get_Object_Value_String(wbemClassObj, L"DriverVersion");
+                gpu.videoMode = _wmi_Instance.get_Object_Value_String(wbemClassObj, L"VideoModeDescription");
+                gpu.memory = _wmi_Instance.get_Object_Value_UI32(wbemClassObj, L"AdapterRAM");
+                // TODO Use registry instead: https://stackoverflow.com/questions/68274009/wmi-win32-videocontroller-ram-4gb-limit
+                gpu.refreshRateMin = _wmi_Instance.get_Object_Value_UI32(wbemClassObj, L"MinRefreshRate");
+                gpu.refreshRateMax = _wmi_Instance.get_Object_Value_UI32(wbemClassObj, L"MaxRefreshRate");
+                gpu.bitsPerPixel = _wmi_Instance.get_Object_Value_UI32(wbemClassObj, L"CurrentBitsPerPixel");
+                _video_Cards.push_back(std::move(gpu));
+                wbemClassObj->Release();
+            }
+        }
+        while (queryResults);
+#elif defined(__unix__)
         // TODO
+#else
+#error "OS not supported"
+#endif
     }
     
     void SystemInfo::_find_Physical_Memory() noexcept {
@@ -91,11 +119,27 @@ namespace love_engine {
     void SystemInfo::_set_Consolidated_System_Info() noexcept {
         // TODO Make a table generator
         std::stringstream buffer;
-        buffer << get_OS() << "\n";
+        buffer << "OS:\n\t" << get_OS();
+        buffer << "\n\nCPUs:";
         for (auto cpu : get_CPUs()) {
-            buffer << cpu.name;
-            buffer << "\n\tCores: " << cpu.cores;
-            buffer << "\n\tThreads: " << cpu.threads;
+            buffer << "\n\t" << cpu.name;
+            buffer << "\n\t\tDescription: " << cpu.description;
+            buffer << "\n\t\tSocket: " << cpu.socket;
+            buffer << "\n\t\tCores: " << cpu.cores;
+            buffer << "\n\t\tThreads: " << cpu.threads;
+            buffer << "\n\t\tMax Speed: " << cpu.speedMax << "MHz";
+            buffer << "\n\t\tLevel 2 Cache Size: " << cpu.l2CacheSize << "KB";
+            buffer << "\n\t\tLevel 3 Cache Size: " << cpu.l3CacheSize << "KB";
+        }
+        buffer << "\n\nVideo Cards:";
+        for (auto gpu : get_Video_Cards()) {
+            buffer << "\n\t" << gpu.name;
+            buffer << "\n\t\tDriver Version: " << gpu.driverVersion;
+            buffer << "\n\t\tVideo Mode Description: " << gpu.videoMode;
+            buffer << "\n\t\tMemory: " << gpu.memory << "B";
+            buffer << "\n\t\tMinimum Refresh Rate: " << gpu.refreshRateMin << "Hz";
+            buffer << "\n\t\tMaximum Refresh Rate: " << gpu.refreshRateMax << "Hz";
+            buffer << "\n\t\tBits per Pixel: " << gpu.bitsPerPixel;
         }
         std::puts(buffer.str().c_str());
         _consolidated_System_Info.assign(buffer.str());
--- src/love/common/system/system_info.hpp
+++ src/love/common/system/system_info.hpp
@@ -18,10 +18,23 @@ namespace love_engine {
 
             typedef struct CPU_Info_ {
                 std::string name;
+                std::string description;
+                std::string socket;
                 uint32_t cores = 0;
                 uint32_t threads = 0;
-                uint32_t speed = 0;
+                uint32_t speedMax = 0;
+                uint32_t l2CacheSize = 0;
+                uint32_t l3CacheSize = 0;
             } CPU_Info;
+            typedef struct Video_Card_Info_ {
+                std::string name;
+                std::string videoMode;
+                std::string driverVersion;
+                uint32_t memory = 0;
+                uint32_t refreshRateMin = 0;
+                uint32_t refreshRateMax = 0;
+                uint32_t bitsPerPixel = 0;
+            } Video_Card_Info;
 
             std::string get_Consolidated_System_Info() noexcept {
                 if (_consolidated_System_Info.empty()) _set_Consolidated_System_Info();
@@ -31,13 +44,13 @@ namespace love_engine {
                 if (_OS_Name.empty()) _find_OS();
                 return _OS_Name;
             }
-            const std::vector<CPU_Info> get_CPUs() noexcept {
+            const std::vector<CPU_Info>& get_CPUs() noexcept {
                 if (_CPUs.empty()) _find_CPUs();
                 return _CPUs;
             }
-            const std::string& get_Video_Card() noexcept {
-                if (_video_Card_Name.empty()) _find_Video_Card();
-                return _video_Card_Name;
+            const std::vector<Video_Card_Info>& get_Video_Cards() noexcept {
+                if (_video_Cards.empty()) _find_Video_Cards();
+                return _video_Cards;
             }
             uint64_t get_Physical_Memory() noexcept {
                 if (_physical_Memory <= 0) _find_Physical_Memory();
@@ -51,13 +64,13 @@ namespace love_engine {
             std::string _consolidated_System_Info;
             std::string _OS_Name;
             std::vector<CPU_Info> _CPUs;
-            std::string _video_Card_Name;
+            std::vector<Video_Card_Info> _video_Cards;
             uint64_t _physical_Memory = 0;
 
             void _set_Consolidated_System_Info() noexcept;
             void _find_OS() noexcept;
             void _find_CPUs() noexcept;
-            void _find_Video_Card() noexcept;
+            void _find_Video_Cards() noexcept;
             void _find_Physical_Memory() noexcept;
     };
 
