4d5f1c0acfd0b44901501ba152960928bac1ab3f
Avery Aaron <business.loveai@gmail.com>
2023-06-25 13:33:13 +0300

    Removed tests, copied to host.exe

--- src/example_game/example_game.cpp
+++ src/example_game/example_game.cpp
@@ -8,25 +8,14 @@
 
 #include <cstdlib>
 
-#include <thread> // TODO Remove
-#include <love/common/system/thread.hpp>
-
 using namespace love_engine;
 using namespace example_game;
-
-void test() {
-    Logger logger(FileIO::get_Executable_Directory() + "../logs/latest.log");
-    logger.log("Hello from test thread!");
-}
 
 int main(int argc, char** argv) {
     LoveEngineInstance::init();
     Logger logger(FileIO::get_Executable_Directory() + "../logs/latest.log", true);
     SystemInfo systemInfo;
     logger.log("System Info:\n" + systemInfo.get_Consolidated_System_Info());
-
-    logger.log("Hello from main thread!");
-    Thread testThread("Test", test);
 
     /*ClientState_Loading loading_State;
     ClientInstance client(&loading_State, 50.f);
--- src/example_game/example_host.cpp
+++ src/example_game/example_host.cpp
@@ -1,5 +1,5 @@
+#include <love/common/love_engine_instance.hpp>
 #include <love/common/data/files/logger.hpp>
-
 #include <love/common/system/system_info.hpp>
 
 #include <cstdlib>
@@ -7,9 +7,11 @@
 using namespace love_engine;
 
 int main(int argc, char** argv) {
-    Logger logger("logs/host_latest.txt");
+    LoveEngineInstance::init();
+    Logger logger(FileIO::get_Executable_Directory() + "../logs/latest.log", true);
     SystemInfo systemInfo;
-    logger.log(Log_Status::LOG_INFO, systemInfo.get_Consolidated_System_Info());
-    
+    logger.log("System Info:\n" + systemInfo.get_Consolidated_System_Info());
+
+    LoveEngineInstance::cleanup();
     exit(EXIT_SUCCESS);
 }
