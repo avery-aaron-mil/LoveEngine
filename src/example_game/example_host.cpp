b0580e3711477da739942b326354855883d6d5e3
Avery Aaron <business.loveai@gmail.com>
2023-07-01 22:32:26 +0300

    System info is not an object

--- src/example_game/example_host.cpp
+++ src/example_game/example_host.cpp
@@ -9,8 +9,7 @@ using namespace love_engine;
 int main(int argc, char** argv) {
     LoveEngineInstance::init();
     Logger logger(FileIO::get_Executable_Directory() + "../logs/latest.log", true);
-    SystemInfo systemInfo;
-    logger.log("System Info:\n" + systemInfo.get_Consolidated_System_Info());
+    logger.log("System Info:\n" + SystemInfo::get_Consolidated_System_Info());
 
     LoveEngineInstance::cleanup();
     exit(EXIT_SUCCESS);
