cbc4c66a1ddd14dea14d820f5f923c1ada125972
Avery Aaron <business.loveai@gmail.com>
2023-07-05 16:41:36 +0300

    Added library function loader

--- src/love/common/system/library.cpp
+++ src/love/common/system/library.cpp
@@ -2,6 +2,7 @@
 
 #include "../error/crash.hpp"
 
+#include <sstream>
 #include <string>
 
 #ifdef __unix__
@@ -9,14 +10,38 @@
 #endif
 
 namespace love_engine {
+    std::string _get_Error() {
+#ifdef _WIN32
+        auto errorCode = GetLastError();
+        LPSTR messageBuffer = nullptr;
+        FormatMessageA(
+            FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_IGNORE_INSERTS,
+            0,
+            errorCode,
+            MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+            messageBuffer,
+            0,
+            nullptr
+        );
+        std::string errorString(messageBuffer);
+        LocalFree(messageBuffer);
+#elif defined(__unix__)
+        const char* errorMsg = std::dlerror();
+        std::string errorString(errorMsg);
+#endif
+        return errorString;
+    }
+
     void Library::load_Library(const char*const library) noexcept {
 #ifdef _WIN32
             _library = LoadLibraryA(library);
 #elif defined(__unix__)
-            _library = dlopen(library);
+            _library = std::dlopen(library);
 #endif
         if (_library == nullptr) {
-            Crash::crash(std::string("Could not load library: ") + library);
+            std::stringstream buffer;
+            buffer << "Could not load library \"" << library << "\": " << _get_Error();
+            Crash::crash(buffer.str());
         }
     }
 
@@ -25,9 +50,29 @@ namespace love_engine {
 #ifdef _WIN32
             FreeLibrary(_library);
 #elif defined(__unix__)
-            dlclose(_library);
+            std::dlclose(_library);
 #endif
             _library = nullptr;
         }
+    }
+
+    Library::function_t Library::load_Library_Function(const char*const function) const noexcept {
+        if (_library == nullptr) {
+            std::stringstream buffer;
+            buffer << "Cannot load library functions before loading library: " << function;
+            Crash::crash(buffer.str());
+        }
+#ifdef _WIN32
+        function_t address = GetProcAddress(_library, function);
+#elif defined(__unix__)
+        function_t address = std::dlsym(_library, function);
+#endif
+        if (address == nullptr) {
+            std::stringstream buffer;
+            buffer << "Could not load library function \"" << function << "\": " << _get_Error();
+            Crash::crash(buffer.str());
+        }
+
+        return address;
     }
 }
--- src/love/common/system/library.hpp
+++ src/love/common/system/library.hpp
@@ -9,8 +9,10 @@ namespace love_engine {
     class Library {
 #ifdef _WIN32
         typedef HMODULE library_t;
+        typedef FARPROC function_t;
 #elif defined(__unix__)
         typedef void* library_t;
+        typedef void* function_t;
 #endif
         public:
             Library() = default;
@@ -23,6 +25,8 @@ namespace love_engine {
 
             void load_Library(const char*const library) noexcept;
             void unload_Library() noexcept;
+            function_t load_Library_Function(const char*const function) const noexcept;
+            // TODO Consider loading all libraries this way to have runtime checks
 
         private:
             library_t _library = nullptr;
