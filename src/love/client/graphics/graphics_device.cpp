43be187c505612442d95db3e338eb1636f2d31a5
Avery Aaron <business.loveai@gmail.com>
2023-07-07 18:53:54 +0300

    GraphicsDevice work
    Now compiles and is mostly done to how I'd like it. Lot of work... Not tested yet since I need to have a surface to check suitability, which requires the Window class to be completed. Soon though! Then I can move on to data serialization like I should be according to the Trello.

--- src/love/client/graphics/graphics_device.cpp
+++ src/love/client/graphics/graphics_device.cpp
@@ -1,23 +1,32 @@
 #include "graphics_device.hpp"
 
 #include <sstream>
+#include <unordered_set>
 
 #include <love/common/error/crash.hpp>
 
 #include "vulkan_functions.hpp"
 
 namespace love_engine {
-    void GraphicsDevice::_log(const std::string& message) const noexcept {
-        if (_settings.logger.get() != nullptr) {
-            _settings.logger.get()->log(message);
+    std::vector<const char*> _enabledExtensions = {
+        VK_KHR_SWAPCHAIN_EXTENSION_NAME
+    };
+
+    void GraphicsDevice::_log(const std::string &message) const noexcept {
+        if (_logger.get() != nullptr) {
+            _logger.get()->log(message);
         }
     }
 
-    void GraphicsDevice::_get_Physical_Devices() noexcept {
+    void GraphicsDevice::addEnabledExtension(const char* extension) const noexcept {
+        _enabledExtensions.push_back(extension);
+    }
+
+    void GraphicsDevice::_getPhysicalDevices() noexcept {
         _log("Getting physical graphics devices...");
 
         uint32_t deviceCount = 0;
-        if (vkEnumeratePhysicalDevices(Vulkan.Instance, &deviceCount, nullptr) != VK_SUCCESS) {
+        if (vkEnumeratePhysicalDevices(_vulkanInstance, &deviceCount, nullptr) != VK_SUCCESS) {
             Crash::crash("Failed to enumerate physical devices.");
         }
         if (deviceCount == 0) {
@@ -25,8 +34,14 @@ namespace love_engine {
         }
 
         _physicalDevices.resize(deviceCount);
-        if (vkEnumeratePhysicalDevices(_vulkanInstance, &deviceCount, _physicalDevices.data()) != VK_SUCCESS ) {
+        if (vkEnumeratePhysicalDevices(_vulkanInstance, &deviceCount, _physicalDevices.data()) != VK_SUCCESS) {
             Crash::crash("Failed to enumerate physical devices.");
+        }
+
+        for (size_t i = 0; i < deviceCount; i++) {
+            VkPhysicalDeviceProperties properties;
+            vkGetPhysicalDeviceProperties(_physicalDevices[i], &properties);
+            _physicalDevicesNameToIndex[properties.deviceName] = i;
         }
 
         std::stringstream buffer;
@@ -34,64 +49,123 @@ namespace love_engine {
         _log(buffer.str());
     }
 
-    std::string _check_Device_Suitability(
-        const VkPhysicalDevice& device,
-        const VkPhysicalDeviceProperties& properties,
-        const VkPhysicalDeviceFeatures& features
-    ) noexcept {
-        if ((VK_VERSION_MAJOR(properties.apiVersion) < 1) &&
-            (properties.limits.maxImageDimension2D < 4096)
-        ) return std::string("maxImageDimension2D < 4096");
+    GraphicsDevice::QueueFamilyIndices GraphicsDevice::_getDeviceQueueFamilies(const VkPhysicalDevice& device) const noexcept {
+        QueueFamilyIndices queueIndices;
+        uint32_t queueFamilyCount = 0;
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+        if (queueFamilyCount == 0) return queueIndices;
 
-        // TODO
-        QueueFamilyIndices queueFamilyIndicies = findQueueFamilies(device);
-        if (!queueFamilyIndicies.is_Complete()) {
-            return std::string("Queue family indicies are not complete.");
-        }
+        std::vector<VkQueueFamilyProperties> queueFamilyProperties(queueFamilyCount);
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, &queueFamilyProperties[0]);
+        for (uint32_t i = 0; i < queueFamilyCount; ++i) {
+            if (queueFamilyProperties[i].queueCount > 0) {
+                // Check if queue can be used for graphics
+                if (queueFamilyProperties[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+                    queueIndices.hasGraphicsQueue = true;
+                    queueIndices.graphicsQueue = i;
+                }
 
-        if (!checkDeviceExtensionSupport(device)) return std::string("Extensions not supported.");
+                // Check if queue can be used for presenting
+                VkBool32 presentSupport = false;
+                vkGetPhysicalDeviceSurfaceSupportKHR(device, i, _surface, &presentSupport);
+                if (presentSupport) {
+                    queueIndices.hasPresentQueue = true;
+                    queueIndices.presentQueue = i;
+                }
 
-        // TODO
-        SwapChainSupportDetails swapChainSupport = querySwapChainSupport(device);
-        if (swapChainSupport.formats.empty()) {
-            return std::string("Swap Chain formats support is empty.");
-        }
-        if (!swapChainSupport.presentModes.empty()) {
-            return std::string("Swap Chain presentModes support is empty.");
-        }
-
-        if (!features.samplerAnisotropy) {
-            return std::string("Sampler anisotropy not supported.");
+                if (queueIndices.hasAllQueues()) break;
+            }
         }
 
-        if (!features.geometryShader) {
-            return std::string("Geometry shaders not supported.");
-        }
-        
-        if (!features.shaderInt64) {
-            return std::string("64-bit integers not supported.");
-        }
-        
-        if (!features.shaderFloat64) {
-            return std::string("64-bit floats not supported.");
+        return queueIndices;
+    }
+    
+    bool GraphicsDevice::_checkDeviceHasEnabledExtensions(const VkPhysicalDevice& device) noexcept {
+        uint32_t extensionCount;
+        vkEnumerateDeviceExtensionProperties(device, nullptr, &extensionCount, nullptr);
+
+        std::vector<VkExtensionProperties> availableExtensions(extensionCount);
+        vkEnumerateDeviceExtensionProperties(
+            device,
+            nullptr,
+            &extensionCount,
+            availableExtensions.data()
+        );
+
+        std::unordered_set<std::string> remainingExtensions(_enabledExtensions.begin(), _enabledExtensions.end());
+        for (const auto &extension : remainingExtensions) {
+            remainingExtensions.erase(extension);
         }
 
+        return remainingExtensions.empty();
+    }
+    
+    GraphicsDevice::SwapChainSupportDetails GraphicsDevice::_querySwapChainSupport(const VkPhysicalDevice& device) const noexcept {
+        SwapChainSupportDetails details;
+        vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device, _surface, &details.capabilities);
+
+        uint32_t formatCount;
+        vkGetPhysicalDeviceSurfaceFormatsKHR(device, _surface, &formatCount, nullptr);
+
+        if (formatCount > 0) {
+            details.formats.resize(formatCount);
+            vkGetPhysicalDeviceSurfaceFormatsKHR(device, _surface, &formatCount, details.formats.data());
+        }
+
+        uint32_t presentModeCount;
+        vkGetPhysicalDeviceSurfacePresentModesKHR(device, _surface, &presentModeCount, nullptr);
+
+        if (presentModeCount > 0) {
+            details.presentModes.resize(presentModeCount);
+            vkGetPhysicalDeviceSurfacePresentModesKHR(
+                device,
+                _surface,
+                &presentModeCount,
+                details.presentModes.data()
+            );
+        }
+
+        return details;
+    }
+
+    std::string GraphicsDevice::_getDeviceUnsuitabilityReason(const VkPhysicalDevice& device) const noexcept {
+        // Check limits
+        VkPhysicalDeviceProperties properties;
+        vkGetPhysicalDeviceProperties(device, &properties);
+        if ((VK_VERSION_MAJOR(properties.apiVersion) < 1) && (properties.limits.maxImageDimension2D < 4096)) {
+            return std::string("maxImageDimension2D < 4096.");
+        }
+
+        // Check features
+        VkPhysicalDeviceFeatures features;
+        vkGetPhysicalDeviceFeatures(device, &features);
+        if (!features.samplerAnisotropy) return std::string("Sampler anisotropy not supported.");
+        if (!features.geometryShader) return std::string("Geometry shaders not supported.");
+        if (!features.shaderInt64) return std::string("64-bit integers not supported.");
+        if (!features.shaderFloat64) return std::string("64-bit floats not supported.");
+
+        // Check queue families
+        QueueFamilyIndices queueIndices = _getDeviceQueueFamilies(device);
+        if (!queueIndices.hasAllQueues()) return std::string("No queues family with required queues found.");
+
+        // Check extension support
+        if (!_checkDeviceHasEnabledExtensions(device)) return std::string("Extensions not supported.");
+
+        // Check swap chain support
+        SwapChainSupportDetails swapChainSupport = _querySwapChainSupport(device);
+        if (swapChainSupport.formats.empty()) return std::string("No swap chain formats supported.");
+        if (swapChainSupport.presentModes.empty()) return std::string("No swap chain present modes supported.");
+
+        // Device suitable; no reason to report
         return std::string();
     }
 
-    int64_t GraphicsDevice::_rate_Device(const VkPhysicalDevice& device) noexcept {
+    int64_t GraphicsDevice::_rateDevice(const VkPhysicalDevice& device) const noexcept {
         VkPhysicalDeviceProperties properties;
-        VkPhysicalDeviceFeatures features;
-
         vkGetPhysicalDeviceProperties(device, &properties);
-        vkGetPhysicalDeviceFeatures(device, &features);
-
-        uint32_t versionMajor = VK_VERSION_MAJOR(properties.apiVersion);
-        uint32_t versionMinor = VK_VERSION_MINOR(properties.apiVersion);
-        uint32_t versionPatch = VK_VERSION_PATCH(properties.apiVersion);
 
         // Fails if not suitable
-        std::string excuse = _check_Device_Suitability(device, properties, features);
+        std::string excuse = _getDeviceUnsuitabilityReason(device);
         if (!excuse.empty()) {
             std::stringstream buffer;
             buffer << "Graphics device \"" << properties.deviceName << "\" failed rating: " << excuse;
@@ -99,19 +173,18 @@ namespace love_engine {
             return -1;
         }
 
-        // Rate
         int64_t score = 0;
 
         // Prefer up-to-date
-        score += 100 * versionMajor;
-        score += 10 * versionMinor;
-        score += versionPatch;
-        
+        score += VK_VERSION_MAJOR(properties.apiVersion) * 10000;
+        score += VK_VERSION_MINOR(properties.apiVersion) * 100;
+        score += VK_VERSION_PATCH(properties.apiVersion);
+
         // Discrete GPUs have a significant performance advantage
-        if (properties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) score += 1000000;
+        if (properties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) score += 1000000000;
 
         // Maximum possible size of textures affects graphics quality
-        score += 10 * properties.limits.maxImageDimension2D;
+        score += properties.limits.maxImageDimension2D * 100; // Will be > 4096
 
         std::stringstream buffer;
         buffer << "Graphics device \"" << properties.deviceName << "\" passed with a rating of: " << score;
@@ -119,28 +192,144 @@ namespace love_engine {
         return score;
     }
 
-    void GraphicsDevice::_create_Device() noexcept {
+    VkDevice GraphicsDevice::_createLogicalDevice(
+        const VkPhysicalDevice& physicalDevice,
+        const VkPhysicalDeviceFeatures& deviceFeatures
+    ) const noexcept {
         _log("Creating Vulkan device...");
-        //
+
+        // Input validation
+        std::string excuse = _getDeviceUnsuitabilityReason(physicalDevice);
+        if (!excuse.empty()) {
+            std::stringstream buffer;
+            buffer << "Could not create logical device because the physical device does not meet requirements: " << excuse;
+            Crash::crash(buffer.str());
+        }
+
+        // Create queue create infos
+        QueueFamilyIndices indices = _getDeviceQueueFamilies(physicalDevice);
+        std::vector<VkDeviceQueueCreateInfo> queueCreateInfos;
+        std::unordered_set<uint32_t> uniqueQueueFamilies = {indices.graphicsQueue, indices.presentQueue};
+
+        float queuePriority = 1.0f;
+        for (uint32_t queueFamily : uniqueQueueFamilies) {
+            VkDeviceQueueCreateInfo queueCreateInfo = {
+                .sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+                .queueFamilyIndex = queueFamily,
+                .queueCount = 1,
+                .pQueuePriorities = &queuePriority,
+            };
+            queueCreateInfos.push_back(queueCreateInfo);
+        }
+
+        // Create device create info
+        VkDeviceCreateInfo deviceCreateInfo = {
+            .sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+            .queueCreateInfoCount = static_cast<uint32_t>(queueCreateInfos.size()),
+            .pQueueCreateInfos = queueCreateInfos.data(),
+            .enabledExtensionCount = static_cast<uint32_t>(_enabledExtensions.size()),
+            .ppEnabledExtensionNames = _enabledExtensions.data(),
+            .pEnabledFeatures = &deviceFeatures
+        };
+
+        // Create device
+        VkDevice device;
+        if ((vkCreateDevice(physicalDevice, &deviceCreateInfo, nullptr, &device) != VK_SUCCESS) ||
+            (device == nullptr)
+        ) {
+            Crash::crash("Failed to create Vulkan device.");
+        }
+
         _log("Created Vulkan device.");
+        return device;
     }
 
-    void GraphicsDevice::_load_Device_Vulkan_Functions() const noexcept {
+    void GraphicsDevice::_loadDeviceVulkanFunctions() const noexcept {
         _log("Loading Vulkan device functions...");
+
         #define VK_GLOBAL_LEVEL_FUNCTION(fun)\
-        if(!(fun = (PFN_##fun) vkGetDeviceProcAddr(_device, #fun))) {\
+        if (!(fun = (PFN_##fun) vkGetDeviceProcAddr(_device, #fun))) {\
             Crash::crash("Could not load Vulkan device function: " #fun);\
         }
         #include "vulkan_functions.inl"
+
         _log("Loaded Vulkan device functions.");
     }
+    
+    VkPhysicalDevice GraphicsDevice::getPhysicalDeviceFromName(const std::string& name) const {
+        // Input validation
+        if (_physicalDevices.empty()) throw std::invalid_argument("No physical graphics devices found.");
+        if (!_physicalDevicesNameToIndex.contains(name)) {
+            std::stringstream buffer;
+            buffer << "No physical graphics device found containing device name: " << name;
+            throw std::invalid_argument(buffer.str());
+        }
 
-    GraphicsDevice::GraphicsDevice(std::shared_ptr<Logger> logger, VkInstance vulkanInstance)
-        : _logger(logger), _vulkanInstance(vulkanInstance)
+        return _physicalDevices[_physicalDevicesNameToIndex.at(name)];
+    }
+
+    VkPhysicalDevice GraphicsDevice::getBestDevice() const {
+        // Input validation
+        if (_physicalDevices.empty()) throw std::invalid_argument("No physical graphics devices found.");
+
+        int64_t bestScore = -1;
+        size_t bestDeviceIndex = 0;
+        int64_t deviceScore;
+        for (size_t i = 0; i < _physicalDevices.size(); i++) {
+            deviceScore = _rateDevice(_physicalDevices[i]);
+            if (deviceScore > bestScore) {
+                bestScore = deviceScore;
+                bestDeviceIndex = i;
+            }
+        }
+
+        return _physicalDevices[bestDeviceIndex];
+    }
+
+    void GraphicsDevice::usePhysicalDevice(const VkPhysicalDevice& device) noexcept {
+        // Input validation
+        if (device == nullptr) {
+            Crash::crash("Null was passed to usePhysicalDevice().");
+        }
+
+        
+        VkPhysicalDeviceFeatures deviceFeatures = {
+            .samplerAnisotropy = VK_TRUE
+        };
+        _device = _createLogicalDevice(device, deviceFeatures);
+        _loadDeviceVulkanFunctions();
+    }
+
+    void GraphicsDevice::_initGraphicsDevice(const std::string& deviceName) noexcept {
+        _getPhysicalDevices();
+        try {
+            VkPhysicalDevice device = deviceName.empty() ? getBestDevice() : getPhysicalDeviceFromName(deviceName);
+            usePhysicalDevice(device);
+        } catch (std::invalid_argument& e) { Crash::crash(e.what()); }
+    }
+
+    GraphicsDevice::GraphicsDevice(std::shared_ptr<Logger> logger, VkInstance vulkanInstance, VkSurfaceKHR surface)
+        : _logger(logger), _vulkanInstance(vulkanInstance), _surface(surface)
     {
-        _get_Physical_Devices();
-        _create_Device();
-        _load_Device_Vulkan_Functions();
+        // Input validation
+        if (_vulkanInstance == nullptr) Crash::crash("Vulkan instance passed to Graphics Device was null.");
+        if (_surface == nullptr) Crash::crash("Surface passed to Graphics Device was null.");
+
+        _initGraphicsDevice("");
+    }
+    GraphicsDevice::GraphicsDevice(
+        std::shared_ptr<Logger> logger,
+        VkInstance vulkanInstance,
+        VkSurfaceKHR surface,
+        const std::string& deviceName
+    )
+        : _logger(logger), _vulkanInstance(vulkanInstance), _surface(surface)
+    {
+        // Input validation
+        if (_vulkanInstance == nullptr) Crash::crash("Vulkan instance passed to Graphics Device was null.");
+        if (_surface == nullptr) Crash::crash("Surface passed to Graphics Device was null.");
+
+        _initGraphicsDevice(deviceName);
     }
 
     GraphicsDevice::~GraphicsDevice() {
--- src/love/client/graphics/graphics_device.hpp
+++ src/love/client/graphics/graphics_device.hpp
@@ -4,6 +4,7 @@
 #include <cstdint>
 #include <memory>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 #include <love/common/data/files/logger.hpp>
@@ -15,20 +16,55 @@
 namespace love_engine {
     class GraphicsDevice {
         public:
-            GraphicsDevice(std::shared_ptr<Logger> logger, VkInstance vulkanInstance);
+            struct QueueFamilyIndices {
+                bool hasGraphicsQueue = false;
+                bool hasPresentQueue = false;
+                uint32_t graphicsQueue;
+                uint32_t presentQueue;
+
+                inline bool hasAllQueues() const noexcept { return hasGraphicsQueue && hasPresentQueue; }
+            };
+            struct SwapChainSupportDetails {
+                VkSurfaceCapabilitiesKHR capabilities;
+                std::vector<VkSurfaceFormatKHR> formats;
+                std::vector<VkPresentModeKHR> presentModes;
+            };
+
+            GraphicsDevice(std::shared_ptr<Logger> logger, VkInstance vulkanInstance, VkSurfaceKHR surface);
+            GraphicsDevice(
+                std::shared_ptr<Logger> logger,
+                VkInstance vulkanInstance,
+                VkSurfaceKHR surface,
+                const std::string& deviceName
+            );
             ~GraphicsDevice();
 
+            void addEnabledExtension(const char* extension) const noexcept;
+            VkPhysicalDevice getBestDevice() const;
+            VkPhysicalDevice getPhysicalDeviceFromName(const std::string& name) const;
+            void usePhysicalDevice(const VkPhysicalDevice& device) noexcept;
+
         private:
-            std::shared_ptr<Logger> logger;
+            std::shared_ptr<Logger> _logger;
             VkInstance _vulkanInstance;
             VkDevice _device;
+            VkSurfaceKHR _surface;
             std::vector<VkPhysicalDevice> _physicalDevices;
+            std::unordered_map<std::string, size_t> _physicalDevicesNameToIndex;
 
             void _log(const std::string& message) const noexcept;
-            void _get_Physical_Devices() noexcept;
-            int64_t _rate_Device(const VkPhysicalDevice& device) noexcept();
-            void _create_Device() noexcept;
-            void _load_Device_Vulkan_Functions() const noexcept;
+            void _getPhysicalDevices() noexcept;
+            QueueFamilyIndices _getDeviceQueueFamilies(const VkPhysicalDevice& device) const noexcept;
+            static bool _checkDeviceHasEnabledExtensions(const VkPhysicalDevice& device) noexcept;
+            SwapChainSupportDetails _querySwapChainSupport(const VkPhysicalDevice& device) const noexcept;
+            std::string _getDeviceUnsuitabilityReason(const VkPhysicalDevice& device) const noexcept;
+            int64_t _rateDevice(const VkPhysicalDevice& device) const noexcept;
+            VkDevice _createLogicalDevice(
+                const VkPhysicalDevice& physicalDevice,
+                const VkPhysicalDeviceFeatures& deviceFeatures
+            ) const noexcept;
+            void _loadDeviceVulkanFunctions() const noexcept;
+            void _initGraphicsDevice(const std::string& deviceName) noexcept;
     };
 }
 
